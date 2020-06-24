add47e28752be1410eb686374191fe5eb884fed8
wonglimey <limey.wong@gmail.com>
2020-06-24 13:59:32 +0800

    init commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+//C:\Java\jdk1.8.0_121\bin\javac HelloWorld.java
+//if the java file contains any compile time errors, 
+//compilation will be failed and list of errors will be displayed on the console. 
+//If the java file does not contain any compile time errors, 
+//compilation will be successful and a .class file will be generated in the same folder.
+//C:\Java\jdk1.8.0_121\bin\java HelloWorld
+//'java' command is used to run the .class files. 
+//Trigger java command, you will get Hello Java world! as output.
+
+class HelloWorld {
+
+    public static void main(String args[]) {
+	    System.out.println("Hello Java world!");
+	}
+	
+}
